057e55753b093bef687df20615c2eb65dbba62f6
Samydulait <Samydulait@github.com>
2022-09-13 12:29:16 -0700

    a program that prints putchar

---
+++ 0x02-function_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
+void print_alphabet(void)
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
