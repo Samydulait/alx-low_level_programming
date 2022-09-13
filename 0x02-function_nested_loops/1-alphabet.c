eb46cef6b5672f233ba0ac3069641dd6219c496e
Samydulait <Samydulait@github.com>
2022-09-13 14:26:12 -0700

    alphabet

--- 0x02-function_nested_loops/1-alphabet.c
+++ 0x02-function_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
-
 /**
   * main - Entry point
   *
   * Return: Always 0 (Success)
-  */
+ */
 
 void print_alphabet(void)
 {
-	for (int i = 'a'; i <= 'z'; i++)
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);
+		_putchar(i);
 	}
-	putchar('\n');
+	_putchar('\n');
 }
