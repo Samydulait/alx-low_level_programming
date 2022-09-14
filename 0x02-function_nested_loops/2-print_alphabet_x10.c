cd45259a15873e55f5d853737159f2e388a4c5bd
Samydulait <Samydulait@github.com>
2022-09-13 17:15:02 -0700

    alphabet

--- 0x02-function_nested_loops/2-print_alphabet_x10.c
+++ 0x02-function_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  * print_alphabet - print_alphabet_x10
+  * print_alphabet_x10 - print_alphabet_x10
   *
   * Return: Always 0 (Success)
   */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 	int j = 0;
