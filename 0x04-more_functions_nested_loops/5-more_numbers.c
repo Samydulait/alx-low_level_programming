3dcebe71c1a80a0c01b6101ae3f50cbb473bbed1
Samydulait <Samydulait@github.com>
2022-09-15 19:39:35 -0700

    collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*more-numbers - prints numbers to 14
+Â*
+*Return: returns nothing
+*/
+
+void more_numbers(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 < 10; n1++)
+	{
+		for (n2 = 0; n2 <= 14; n2++)
+		{
+
+			if (n2 > 9)
+			{
+				_putchar((n2 / 10) + '0');
+			}
+			_putchar((n2 % 10Â) + '0');
+		}
+		_putchar(10Â°.
+	}
+}															}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+Â*print_square - prints squares
+*@size: parameter
+*Return: returns nothing
+*/
+
+vois print_ square(int size)
+{
+	int inc1, inc2;
+
+	if (size > 0)
+	{
+		for (inc1 = 0; inc1 < size; inc1++)
+		{
+
+			for (inc2 = 0; inc2 < (size -1); inc2++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
