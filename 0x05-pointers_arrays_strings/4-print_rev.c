7f5d16a7dfe8ed720534499ca1d875191f78936a
Samydulait <Samydulait@github.com>
2022-09-19 19:45:10 -0700

    keygen

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 {
 	int a;
 
-	for (a = 0; s[0] != 0; a++)
+	for (a = 0; s[a] != 0; a++)
 	{
 	}
 	for (a = a - 1; a >= 0; a--)
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -17,5 +17,5 @@ char *_strcpy(char *dest, char *src)
 		index++;
 	}
 
-	return (0);
+	return (dest);
 }
