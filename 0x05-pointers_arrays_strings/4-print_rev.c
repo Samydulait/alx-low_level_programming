8cf1eacc164f8ea4e8df823d145dd8edb661c473
Samydulait <Samydulait@github.com>
2022-09-19 18:47:47 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,13 @@
 
 void print_rev(char *s)
 {
-	int len = 0, index;
+	int a;
 
-	while (s[index++])
-		len++;
+	for (a = 0; s[0] != 0; a++)
+	{
+	}
+	for (a = a - 1; a >= 0; a--)
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
-
+	_putchar(s[a]);
 	_putchar('\n');
 }
