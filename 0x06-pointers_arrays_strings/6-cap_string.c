56297321900092ce3b904e3bb467e45b0c170269
Samydulait <Samydulait@github.com>
2022-09-22 06:08:52 -0700

    Pointer, array and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,35 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
-  * isLower - Determines whether ascii is lowercase
-  * @c: character
-  *
-  * Return: 1 if true, 0 if false
-  */
-
-int isLower(char c)
-{
-	return (c >= 97 && c <= 122);
-}
-
-/**
-  * isDelimiter - Determines whether ascii is a delimiter
-  * @c: character
-  *
-  * Return: 1 if true, 0 if false
-  */
-
-int isDelimiter(char c)
-{
-	int i;
-	char delimiter[] = "\t\n,.!?(){}";
-
-	for (i = 0; i < 12; i++)
-		if (c == delimiter[i])
-			return (1);
-	return (0);
-}
 
 /**
   * cap_string - Capitalizes all words of a string
@@ -40,21 +9,25 @@ int isDelimiter(char c)
 
 char *cap_string(char *s)
 {
-	char *ptr = s;
-	int foundDelimit = 1;
+	int count = 0, i;
+	int separator[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	while (*s)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+
+	while (*(s + count) != '\0')
 	{
-		if (isDelimiter(*s))
-			foundDelimit = 1;
-		else if (isLower(*s) && foundDelimit)
+		for (i = 0; i < 13; i++)
 		{
-			*s -= 32;
-			foundDelimit = 0;
+			if (*(s + count) == separator[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+				*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
 		}
-		else
-			foundDelimit = 0;
-		s++;
+		count++;
 	}
-	return (ptr);
+	return (s);
 }
